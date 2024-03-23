57f917f1b0066cd6248175b251aa9389aa8bce4c
dev atal <134199050+ataldev887@users.noreply.github.com>
2024-03-23 13:35:28 +0530

    handling multiple servers

--- nodejs/serverr.js
+++ nodejs/serverr.js
@@ -2,6 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req,res) => {
     console.log(req.url);
+    res.write('Welcome to my application ');
     if(req.url=='/product'){
         return res.end('This is a product page');
     }else if(req.url=='/user'){
