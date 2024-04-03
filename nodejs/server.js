1c23cf6ea6ea028164de39ade74ee8a7dc38940f
dev atal <134199050+ataldev887@users.noreply.github.com>
2024-04-03 09:28:38 +0530

    First Server

--- nodejs/server.js
+++ nodejs/server.js
@@ -7,7 +7,7 @@ const http = require('http');
 //2) Create Server
 const server = http.createServer((req , res) => {
     //Here comes the request
-    res.end('Welcome to the Nodejs Server ');
+    res.end('Welcome to the Nodejs Servers ');
 });
 
 //3) Specify a port to listen to client's requests
