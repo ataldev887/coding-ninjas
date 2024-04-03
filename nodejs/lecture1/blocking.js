ef2ea2687192d3357a58d86f30238525cdbec93c
dev atal <134199050+ataldev887@users.noreply.github.com>
2024-04-03 19:01:40 +0530

    Blocking in NodeJs

---
+++ nodejs/lecture1/blocking.js
@@ -0,0 +1,7 @@
+for(let i=0;i<10000000000;i++){
+    //I am doing nothing just consuming time
+}
+
+console.log('Task to be performed after loop');
+
+console.log('I have to wait for this long loop to finish :( ');
